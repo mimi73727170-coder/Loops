bc9672af3d027268e6495372b79e1961eb0fb04d
Mimi <mimi73727170@gmail.com>
2026-07-20 19:22:19 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,17 @@
+#Task1
+for i in range(5) :
+    print("Python")
+
+#Task2
+for i in range(10,0,-1) :
+    print(i)
+
+#Task3
+number = int(input("Enter a number :"))
+for i in range(1,number +1) :
+    print(i)
+
+#Challenge
+number = int(input("Enter a number :"))
+for i in range(1,11) :
+    print(number,"X",i,"=",number*i)
